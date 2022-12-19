ffc1e41d57db6f6b99823345f7ff70813b7e34c0
Andrew Subotin <subotinandrey5@gmail.com>
2022-12-19 16:37:08 +0200

    results

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -33,8 +33,13 @@ def vote(request: HttpRequest, question_id):
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         return render(request, 'polls/detail.html', {'question': question, 'error_message': "You didn't select a choice."})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
 
-    selected_choice.votes += 1
-    selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id, )))
 
-    return HttpResponseRedirect(reverse('polls:results', args=(question.id, )))
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
