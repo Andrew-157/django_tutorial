89d36bbe1c272d85481f2f3bcda69fb2549009e0
Andrew Subotin <subotinandrey5@gmail.com>
2022-12-21 08:49:09 +0200

    change index to display only appropriate questions

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,8 @@
-from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
-from django.template import loader
+from django.http import HttpRequest, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
@@ -12,7 +12,13 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        Return the last five recent questions (not including those set
+        to be published in the future)
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
